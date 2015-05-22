e707c06418073e3a9d5b92e3911cde2b04f9711e
smallilies <smallilies@gmail.com>
2015-05-22 06:12:36 +0530

    -smallilies_test

--- index.js
+++ index.js
@@ -9,15 +9,3 @@ globalize.smallilies = function globalize_smallilies() {
     for (key in smallilies)
         globalize(key, smallilies[key]);
 }
-
-// (function smallilies_test() {(function() {var a1 = {a1: 'a1', a2: 'a1', b1: {b1: 'b1', }, }; console.log('a1:', a1); var a2 = {a2: 'a2', b1: {b2: 'b2', b2r: 'b2r', }, }; var c1 = extend(true, a1, a2); console.log('a1:', a1); console.log('a2:', a2); console.log('c1:', c1); })(); })();
-// (function smallilies_test() {
-//     (function() {
-//         var a1;
-//         var a2 = {a2: 'a2', b1: {b2: 'b2', b2r: 'b2r', }, };
-//         var c1 = extend(true, a1, a2);
-//         console.log('c1:', c1);
-//         // console.log('a2:', a2);
-//         // console.log('c1:', c1);
-//     })();
-// })();
