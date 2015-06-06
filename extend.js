3763aefc83ab81ac9a82bbed3d8a2ab4a6af6615
smallilies <smallilies@gmail.com>
2015-06-06 14:31:23 +0530

    remove extend

--- extend.js
+++ extend.js
@@ -6,50 +6,45 @@ module.exports = extend;
 //     clone: false,
 // };
 
+function isOptions(options) {
+    if (typeof options == 'boolean') return true;
+    if (typeof options.deep == 'boolean') return true;
+    if (typeof options.replace == 'boolean') return true;
+    if (typeof options.clone == 'boolean') return true;
+}
+
 function extend(options, dest, src1, src2 /*...*/ ) {
     var args = [].slice.call(arguments);
 
-    if (typeof options == 'boolean')
-        options = {
-            deep: options
-        };
-    else if (options && (
-            (options.deep && typeof options.deep == 'boolean') ||
-            (options.replace && typeof options.replace == 'boolean')
-        ))
-        options = options;
-    else
-        args.unshift(options = {});
+    if (!isOptions(options)) args.unshift(options = {});
 
-
-    // If no src(s), return dest as is
-    if (args.length < 3) return dest;
-
-    var dest = options.clone ? JSON.parse(JSON.stringify(args[1])) : args[1];
-    if (!dest) dest = {};
-
+    var dest = args[1] || {};
     var srcs = args.slice(2);
-
-    // console.log('options:', options);
-    // console.log('dest:', dest);
-    // console.log('srcs:', srcs);
 
     srcs.forEach(function copy(src, i) {
         try {
-            for (key in src)(function forOfKeys(key, value, src) {
+            for (key in src)/*(function(key, value, src)*/ {
                 try {
-                    if (!dest[key])
+                    if (!dest[key]){
+                        // console.log(1);
                         dest[key] = src[key];
-                    else if (typeof src[key] == 'object' && typeof dest[key] == 'object')
+                    }
+                    else if (typeof src[key] == 'object' && typeof dest[key] == 'object'){
+                        try{
+                        console.log('\n==', 2, key, '\noptions', options, '\nsrc', src[key], '\ndest', dest[key]);
+                        }catch(err){console.error(err);}
                         extend(options, dest[key], src[key]);
-                    else if (options.replace)
+                    }
+                    else if (options.replace){
+                        console.log(3);
                         dest[key] = src[key];
+                    }
                     // console.log('dest[' + key + ']', dest[key]);
                 } catch (err) {
-                    err.message = 'Error in src' + i + '[' + key + ']:"' + value + '". ' + err.message;
+                    err.message = 'Error in src' + i + '[' + key + ']:"' + src[key] + '". ' + err.message;
                     console.error(err);
                 };
-            })(key, src[key], src);
+            }/*)(key, src[key], src)*/;
         } catch (err) {
             err.message = 'Error in src' + i + '. ' + err.message;
             console.error(err);
