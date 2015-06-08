3a36bc2d70dd7d15f148f8991685c5af31ab19c9
smallilies <smallilies@gmail.com>
2015-06-08 10:54:38 +0530

    globalize undefined false

--- globalize.js
+++ globalize.js
@@ -1,15 +1,16 @@
 var defineProperty = require('./define-property');
 
 function globalize(name, thing) {
-    if (name && thing) {
-        defineProperty(global, name, function() {
-            return thing;
-        });
-    } else {
+    if (!arguments.length) {
         var smallilies = require('./');
         for (key in smallilies)
             globalize(key, smallilies[key]);
+        return;
     }
+    if (name && name.length && (typeof thing != 'undefined'))
+        defineProperty(global, name, function() {
+            return thing;
+        });
 }
 
 module.exports = globalize;
