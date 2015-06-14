f2d8bfec5bcb0e8ff99809c69d4983d2696db072
smallilies <smallilies@gmail.com>
2015-06-14 19:35:17 +0530

    matchIndexOf

--- match-index-of.js
+++ match-index-of.js
@@ -1,5 +1,6 @@
 function matchIndexOf(array, value) {
     for (var i = array.length - 1; i >= 0; i--)
         if (array[i].match(value)) return i;
+    return -1;
 };
 module.exports = matchIndexOf;
