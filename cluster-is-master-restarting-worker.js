8a5e1df8ebb2fe9908644f06a779122d3c5e2789
smallilies <smallilies@gmail.com>
2015-06-25 09:33:57 +0530

    isMaster$(forks)

--- cluster-is-master-restarting-worker.js
+++ cluster-is-master-restarting-worker.js
@@ -1,13 +1,13 @@
 var cluster = require('cluster');
 
-function isMasterRestartingWorker() {
+function isMasterRestartingWorker(forks) {
     if (cluster.isMaster) {
         cluster.on('exit', function(worker, code, signal) {
-            console.log('Process %d died (%s). restarting...',
-                worker.process.pid, signal || code);
+            console.log('Process %d died (%s). restarting...', worker.process.pid, signal || code);
             cluster.fork();
         });
-        cluster.fork();
+        if (isNaN(forks) || !forks) forks = 1;
+        for (var i = 1; i <= forks; i++) cluster.fork();
         return true;
     } else return false;
 };
@@ -15,3 +15,4 @@ module.exports = isMasterRestartingWorker;
 
 var defineProperty = require('./define-property');
 defineProperty(cluster, 'isMasterRestartingWorker', isMasterRestartingWorker);
+cluster.isMaster$ = isMasterRestartingWorker;
