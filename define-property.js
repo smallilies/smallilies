535c0b281bd807ca9de13b68db137a069474adbf
smallilies <smallilies@gmail.com>
2015-09-30 07:31:19 +0530

    defineProperty

--- define-property.js
+++ define-property.js
@@ -1,10 +1,17 @@
-function defineProperty(object, name, getter, setter) {
-    Object.defineProperty(object, name, {
-        get: getter,
-        set: setter,
-        configurable: true,
-        enumerable: true,
-    });
-};
-
 module.exports = defineProperty;
+
+function defineProperty(object, name, getter, setter, configurable, enumerable) {
+    var options = {
+        get: () => this['__' + name],
+        // set: (v) => this['__' + name] = v,
+        configurable: true,
+        enumerable: false,
+    };
+    if (getter instanceof Function) options.get = getter;
+    if (setter instanceof Function) options.set = setter;
+    if (setter instanceof Boolean) options.configurable = options.enumerable = setter;
+    if (configurable instanceof Boolean) options.configurable = configurable;
+    if (enumerable instanceof Boolean) options.enumerable = enumerable;
+    Object.defineProperty(object, name, options);
+    return object;
+};
