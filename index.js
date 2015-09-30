fd4c9944bdf6bbcf65dadbe7a1eb4e19f3e23611
smallilies <smallilies@gmail.com>
2015-09-30 07:30:43 +0530

    Promise.some1 = Promise.some concurrency: 1

--- index.js
+++ index.js
@@ -48,3 +48,9 @@ s.Promise.promisifyAll(s.fs);
 
 s.defineProperty(s, 'cwd'   , process.cwd);
 s.defineProperty(s, 'tmpdir', s.os.tmpdir);
+
+s.Promise.some1 = function(arr) {
+    return s.Promise.some(arr, 1, {
+        concurrency: 1
+    });
+};
